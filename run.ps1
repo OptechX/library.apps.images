f58254cadf41f736af576a1d03552b345bac12f5
danijeljw-rpc <danijel@repasscloud.com>
2023-05-03 01:23:17 +1000

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -26,7 +26,7 @@ Get-ChildItem -Path ./_tmp -Recurse -File -Filter "*.png" | ForEach-Object {
 }
 
 # clean up
-if (Test-Path -Path _tmp){ Remove-Item -Path _tmp -Recurse -Confirm:$false -Force }
+if (Test-Path -Path _tmp){ Remove-Item -Path _tmp -Recurse -Force -Confirm:$false }
 
 # convert all missing PNG files (this is from old files not in the source repo)
 $inkscape = $(which inkscape)
@@ -42,3 +42,6 @@ Get-ChildItem -Path . -Recurse -Filter "*.png" | ForEach-Object {
     $new = $_.FullName.Replace(".png","_scaled.png")
     & $(which python) main.py $original $new
 }
+
+# clean up my last mistake
+Get-ChildItem -Path . -Recurse -Filter "*_scaled_scaled*" | ForEach-Object { Remove-Item -Path $_.FullName -Force -Confirm:$false }
